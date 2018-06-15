f500a1f1d2187ebdf68ce6d4db58d13a36c88ecc
Charpell <charpellumeh@gmail.com>
2018-06-15 21:43:59 +0100

    Displaying Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Comments = React.createClass({
+
+  render() {
+    return (
+      <div className="comments">
+        I am the comment
+      </div>
+    )
+  }
+});
+
+export default Comments;
